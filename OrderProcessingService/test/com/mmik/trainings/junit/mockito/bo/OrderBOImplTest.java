52d6fa10bbb1526b5c238754c02d811cdc296af9
mehdimahmoud <mahmoudmehdi@yahoo.fr>
2016-12-08 16:56:09 +0000

    Testing negative scenario for placeOrder method

--- OrderProcessingService/test/com/mmik/trainings/junit/mockito/bo/OrderBOImplTest.java
+++ OrderProcessingService/test/com/mmik/trainings/junit/mockito/bo/OrderBOImplTest.java
@@ -21,13 +21,22 @@ public class OrderBOImplTest {
 	// I ask to mock my DAO interface
 	@Mock
 	OrderDAO dao;
+	private OrderBOImpl orderBO;
 
 	@Before
 	public void setup(){
 		// I initialize to take into account the Mockito annotations into this class :
 		MockitoAnnotations.initMocks(this);
+
+		orderBO = new OrderBOImpl();
+		orderBO.setDao(dao);
 	}
 	
+	/**
+	 * Positive scenario
+	 * @throws SQLException
+	 * @throws BOException
+	 */
 	@Test
 	public void placeOrder_Should_Create_An_Order() throws SQLException, BOException{
 		OrderBOImpl orderBO = new OrderBOImpl();
@@ -42,6 +51,28 @@ public class OrderBOImplTest {
 		// I test the placeOrder call :
 		boolean result = orderBO.placeOrder(order);
 		assertTrue(result);
+		
+		// Verify the stubbed method is called once :
+		verify(dao).create(order);
+		
+	}
+	
+	/**
+	 * Negative scenario
+	 * @throws SQLException
+	 * @throws BOException
+	 */
+	@Test
+	public void placeOrder_Should_Not_Create_An_Order() throws SQLException, BOException{
+		
+		// Stubbing and setting expectations :
+		// call the "When static method to stub (= to replace temporary) and thenReturn to mock (= to simulate) the value return 
+		Order order = new Order();
+		when(dao.create(order)).thenReturn(new Integer(0));
+		
+		// I test the placeOrder call :
+		boolean result = orderBO.placeOrder(order);
+		assertFalse(result);
 		
 		// Verify the stubbed method is called once :
 		verify(dao).create(order);
