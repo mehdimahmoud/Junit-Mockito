9ab0bc37402cea06395ccd430a331a833e81a081
mehdimahmoud <mahmoudmehdi@yahoo.fr>
2016-12-08 17:15:40 +0000

    Mocking Exception scenario for placeOrder method

--- OrderProcessingService/test/com/mmik/trainings/junit/mockito/bo/OrderBOImplTest.java
+++ OrderProcessingService/test/com/mmik/trainings/junit/mockito/bo/OrderBOImplTest.java
@@ -24,59 +24,82 @@ public class OrderBOImplTest {
 	private OrderBOImpl orderBO;
 
 	@Before
-	public void setup(){
+	public void setup() {
 		// I initialize to take into account the Mockito annotations into this class :
 		MockitoAnnotations.initMocks(this);
 
 		orderBO = new OrderBOImpl();
 		orderBO.setDao(dao);
 	}
-	
+
 	/**
 	 * Positive scenario
+	 * 
 	 * @throws SQLException
 	 * @throws BOException
 	 */
 	@Test
-	public void placeOrder_Should_Create_An_Order() throws SQLException, BOException{
+	public void placeOrder_Should_Create_An_Order() throws SQLException, BOException {
 		OrderBOImpl orderBO = new OrderBOImpl();
-		// I passe the mocked "dao" variable 
+		// I passe the mocked "dao" variable
 		orderBO.setDao(dao);
-		
+
 		// Stubbing and setting expectations :
-		// call the "When static method to stub (= to replace temporary) and thenReturn to mock (= to simulate) the value return 
+		// call the "When static method to stub (= to replace temporary) and
+		// thenReturn to mock (= to simulate) the returned value
 		Order order = new Order();
 		when(dao.create(order)).thenReturn(new Integer(1));
-		
+
 		// I test the placeOrder call :
 		boolean result = orderBO.placeOrder(order);
 		assertTrue(result);
-		
-		// Verify the stubbed method is called once :
+
+		// Verify the stubbed method is called at least once :
 		verify(dao).create(order);
-		
+
 	}
-	
+
 	/**
 	 * Negative scenario
+	 * 
 	 * @throws SQLException
 	 * @throws BOException
 	 */
 	@Test
-	public void placeOrder_Should_Not_Create_An_Order() throws SQLException, BOException{
-		
+	public void placeOrder_Should_Not_Create_An_Order() throws SQLException, BOException {
+
 		// Stubbing and setting expectations :
-		// call the "When static method to stub (= to replace temporary) and thenReturn to mock (= to simulate) the value return 
+		// call the "When static method to stub (= to replace temporary) and
+		// thenReturn to mock (= to simulate) the returned value
 		Order order = new Order();
 		when(dao.create(order)).thenReturn(new Integer(0));
-		
+
 		// I test the placeOrder call :
 		boolean result = orderBO.placeOrder(order);
 		assertFalse(result);
-		
-		// Verify the stubbed method is called once :
+
+		// Verify the stubbed method is called at least once :
 		verify(dao).create(order);
-		
+
+	}
+
+	/**
+	 * Mocking exception scenario
+	 * 
+	 * @throws SQLException
+	 * @throws BOException
+	 */
+	@Test(expected = BOException.class)
+	public void placeOrder_Should_Throw_BOException_When_Create_An_Order() throws SQLException, BOException {
+
+		// Stubbing and setting expectations :
+		// call the "When static method to stub (= to replace temporary) and
+		// thenReturn to mock (= to simulate) the returned value
+		Order order = new Order();
+		when(dao.create(order)).thenThrow(SQLException.class);
+
+		// I test the placeOrder call to :
+		boolean result = orderBO.placeOrder(order);
 	}
 
 }
