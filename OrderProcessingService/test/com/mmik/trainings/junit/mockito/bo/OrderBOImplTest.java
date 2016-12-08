86cabc1970944db964376f20f0374798d1b8de40
mehdimahmoud <mahmoudmehdi@yahoo.fr>
2016-12-08 16:43:45 +0000

    Stubbing, setting expectations and verifing

--- OrderProcessingService/test/com/mmik/trainings/junit/mockito/bo/OrderBOImplTest.java
+++ OrderProcessingService/test/com/mmik/trainings/junit/mockito/bo/OrderBOImplTest.java
@@ -1,14 +1,18 @@
 package com.mmik.trainings.junit.mockito.bo;
 
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
 
 import java.sql.SQLException;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import com.mmik.trainings.junit.mockito.bo.exception.BOException;
 import com.mmik.trainings.junit.mockito.dao.OrderDAO;
 import com.mmik.trainings.junit.mockito.dto.Order;
 
@@ -20,19 +24,28 @@ public class OrderBOImplTest {
 
 	@Before
 	public void setup(){
-		// I initialize to take into account the Mockito annotations :
+		// I initialize to take into account the Mockito annotations into this class :
 		MockitoAnnotations.initMocks(this);
 	}
 	
 	@Test
-	public void placeOrder_Should_Create_An_Order() throws SQLException{
+	public void placeOrder_Should_Create_An_Order() throws SQLException, BOException{
 		OrderBOImpl orderBO = new OrderBOImpl();
-		// I passe the mocked dao variable 
+		// I passe the mocked "dao" variable 
 		orderBO.setDao(dao);
 		
+		// Stubbing and setting expectations :
+		// call the "When static method to stub (= to replace temporary) and thenReturn to mock (= to simulate) the value return 
 		Order order = new Order();
-		// static When method used to stub (= to replace temporary) and thenReturn to mock (= to simulate) the value return 
 		when(dao.create(order)).thenReturn(new Integer(1));
+		
+		// I test the placeOrder call :
+		boolean result = orderBO.placeOrder(order);
+		assertTrue(result);
+		
+		// Verify the stubbed method is called once :
+		verify(dao).create(order);
+		
 	}
 
 }
