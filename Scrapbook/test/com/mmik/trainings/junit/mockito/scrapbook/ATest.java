2ff1eb36803ab2b754ad8dff37c728ed00696e78
mehdimahmoud <mahmoudmehdi@yahoo.fr>
2016-12-09 18:30:01 +0000

    Scrapbook : add library, testing A method that calls the void method from the mocked class B

---
+++ Scrapbook/test/com/mmik/trainings/junit/mockito/scrapbook/ATest.java
@@ -0,0 +1,31 @@
+package com.mmik.trainings.junit.mockito.scrapbook;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import static org.mockito.Mockito.*;
+
+public class ATest {
+
+	@Mock
+	private B b;
+	
+	private A a;
+	
+	@Before
+	public void setup(){
+		MockitoAnnotations.initMocks(this);
+		a = new A(b);
+	}
+	
+	@Test
+	public void usesVoidMethod_Should_Call_VoidMethod() throws Exception {
+		// Nothing to stub and to expect when the dependent method is void type
+		a.usesVoidMethod(); // voidMethod method from
+		verify(b).voidMethod();
+	}
+
+}
