bbabab4931ed872d4b3365c674a984a66d579ed5
mehdimahmoud <mahmoudmehdi@yahoo.fr>
2016-12-09 19:41:06 +0000

    Testing multiple consecutive calls of the voidMethod in the same test method

--- Scrapbook/test/com/mmik/trainings/junit/mockito/scrapbook/ATest.java
+++ Scrapbook/test/com/mmik/trainings/junit/mockito/scrapbook/ATest.java
@@ -24,7 +24,7 @@ public class ATest {
 	public void usesVoidMethod_Should_Call_VoidMethod() throws Exception {
 		// the voidMethod can be stubbed implicitly : (nothing to do)
 		// or explicitly like this :
-		doNothing().when(b).voidMethod(); // do nothing when the voidMethod from the mocked class B is invoked.
+		doNothing().when(b).voidMethod(); // do nothing (= return nothing) when the voidMethod from the mocked class B is invoked.
 		// The B voidMethod method is called within the A usesVoidMethod method
 		assertSame(1, a.usesVoidMethod());  
 		verify(b).voidMethod();
@@ -32,8 +32,17 @@ public class ATest {
 	
 	@Test(expected = RuntimeException.class)
 	public void usesVoidMethod_should_Throw_RuntimeException() throws Exception{
-		doThrow(Exception.class).when(b).voidMethod();
+		doThrow(Exception.class).when(b).voidMethod(); // throw Exception when the voidMethod from the mocked class B is invoked.
 		a.usesVoidMethod();
+	}
+
+	@Test(expected = RuntimeException.class)
+	public void usesVoidMethod_ConsecutiveCallsTest() throws Exception{
+		doNothing().doThrow(Exception.class).when(b).voidMethod(); // we tell that the voidMethod is called twice
+		a.usesVoidMethod(); // first one doNothing
+		verify(b).voidMethod();
+		a.usesVoidMethod(); // second one doThrow
+		verify(b).voidMethod();
 	}
 	
 
