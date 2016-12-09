136c66bad8b306b5c2f7586856d3002c32bc6fb0
mehdimahmoud <mahmoudmehdi@yahoo.fr>
2016-12-09 17:50:24 +0000

    Add ScrapBook project to github

---
+++ Scrapbook/src/com/mmik/trainings/junit/mockito/scrapbook/A.java
@@ -0,0 +1,22 @@
+package com.mmik.trainings.junit.mockito.scrapbook;
+
+public class A {
+
+	private B b;
+	
+	public A(B b) {
+		// TODO Auto-generated constructor stub
+		this.b = b;
+	}
+	
+	public int usesVoidMethod(){
+		try {
+			b.voidMethod();
+		} catch (Exception e) {
+			throw new RuntimeException();
+		}
+		
+		return 0;
+	}
+
+}
---
+++ Scrapbook/src/com/mmik/trainings/junit/mockito/scrapbook/B.java
@@ -0,0 +1,13 @@
+package com.mmik.trainings.junit.mockito.scrapbook;
+
+public class B {
+
+	public B() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void voidMethod() throws Exception{
+		//
+	}
+
+}
