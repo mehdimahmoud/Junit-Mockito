29ff2fc5cdfe366fa60f9b06f7ed3396c4035c70
mehdimahmoud <mehdi.mahmoud@gmail.com>
2016-12-13 03:07:42 +0000

    Add dependencies injection by Component and Autowired annotation in the appropriate classes

--- SpringJunit/src/main/java/com/mmik/trainings/junit/mockito/spring/dao/TicketDAOImpl.java
+++ SpringJunit/src/main/java/com/mmik/trainings/junit/mockito/spring/dao/TicketDAOImpl.java
@@ -1,7 +1,11 @@
 package com.mmik.trainings.junit.mockito.spring.dao;
 
+import org.springframework.stereotype.Component;
+
 import com.mmik.trainings.junit.mockito.spring.dto.Ticket;
 
+// 1. Mark by annotation which component to inject
+@Component
 public class TicketDAOImpl implements TicketDAO {
 
 	public int createTicket(Ticket ticket) {
--- SpringJunit/src/main/java/com/mmik/trainings/junit/mockito/spring/service/TicketServiceImpl.java
+++ SpringJunit/src/main/java/com/mmik/trainings/junit/mockito/spring/service/TicketServiceImpl.java
@@ -1,10 +1,17 @@
 package com.mmik.trainings.junit.mockito.spring.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import com.mmik.trainings.junit.mockito.spring.dao.TicketDAO;
 import com.mmik.trainings.junit.mockito.spring.dto.Ticket;
 
+//1. Mark by annotation which component should be receive the injection :
+@Component
 public class TicketServiceImpl implements TicketService {
 
+	// 2. Mark which object to inject :
+	@Autowired
 	private TicketDAO ticketDao;
 
 	public int buyTicket(String passengerName, int phone) {
